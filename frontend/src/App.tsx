208b257e3bf7620b2ac1576299556195312ad0d1
SoulMelody <SoulMelody@users.noreply.github.com>
2026-02-07 16:51:43 +0800

    wip: add sidecar executor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -69,7 +69,7 @@ import { AboutPage } from "./AboutPage";
 import { ConverterPage } from "./ConverterPage";
 import { path } from '@tauri-apps/api';
 import { listen } from "@tauri-apps/api/event";
-import { getCurrentWebview } from "@tauri-apps/api/webview";
+import { Command } from "@tauri-apps/plugin-shell";
 import { ask, open } from '@tauri-apps/plugin-dialog';
 import { writeText } from '@tauri-apps/plugin-clipboard-manager';
 import { revealItemInDir } from '@tauri-apps/plugin-opener';
@@ -220,6 +220,11 @@ export function App(props: Props) {
         }
       }
     })
+
+    const executeSidecar = async () => {
+      await Command.sidecar("libresvip-tauri-server", []).execute();
+    }
+    executeSidecar();
 
     const getAppVersion = async () => {
       setAppVersion(await pyInvoke('app_version', {}));
