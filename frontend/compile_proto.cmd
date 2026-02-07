1e9e3959f8f98206d3882b274c48cdedcfc7cad4
SoulMelody <SoulMelody@users.noreply.github.com>
2026-02-07 17:28:18 +0800

    add command for protobuf compilation

---
+++ frontend/compile_proto.cmd
@@ -0,0 +1,2 @@
+set PATH=%PATH%;%CD%\node_modules\.bin
+protoc --ts_proto_out=./src ./src/assets/libresvip.proto
---
+++ frontend/compile_proto.sh
@@ -0,0 +1,2 @@
+export PATH=$PATH:./node_modules/.bin
+protoc --ts_proto_out=./ ./src/assets/libresvip.proto
